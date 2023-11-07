48804e77964918a4b3167c6ab611fbfd260bd06f
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-11-07 11:37:29 +0100

    add link to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,19 +78,19 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/EmaSuriano/berlin-latino-hub"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+            Open Repo{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Jump into Github and start coding
           </p>
         </a>
       </div>
