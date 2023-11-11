834377e688812f17c3bfa7e8e5dfee6a4134479a
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-11-11 23:37:21 +0100

    test for the win

---
+++ tests/example.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from "@playwright/test";
+import fs from "fs";
+import path from "path";
+
+function isPageFile(file: string) {
+  return file.endsWith("page.tsx");
+}
+
+function displayPageRoutes(directoryPath: string) {
+  function traverseDirectory(currentPath: string): string[] {
+    const files = fs.readdirSync(currentPath);
+
+    const asd = files.flatMap((file) => {
+      const filePath = path.join(currentPath, file);
+
+      if (fs.statSync(filePath).isDirectory()) {
+        // avoid dynamic routes
+        if (filePath.endsWith("[id]")) {
+          return [];
+        }
+        return traverseDirectory(filePath);
+      } else if (isPageFile(file)) {
+        return filePath;
+      }
+
+      return [];
+    });
+
+    return asd;
+  }
+
+  return traverseDirectory(directoryPath);
+}
+
+const routes = displayPageRoutes("./src/app").map((x) =>
+  x.replace("src/app", "").replace("page.tsx", ""),
+);
+
+routes.forEach((route) => {
+  test(`Route ${route}`, async ({ page }) => {
+    await page.goto(`.${route}`);
+
+    // Expect a title "to contain" a substring.
+    await expect(page.getByText("Vamos Berlin!")).toBeInViewport();
+  });
+});
