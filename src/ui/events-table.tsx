57322429ed5c709753d3e890a2b9951bc6426174
Carolina <carolina.acuna90@gmail.com>
2023-11-11 18:47:34 +0100

    form description ok

--- src/ui/events-table.tsx
+++ src/ui/events-table.tsx
@@ -3,7 +3,13 @@
 import { fetchEvents } from "@/lib/actions";
 import { Event } from "@/lib/schema";
 
-const TABLE_ROWS: (keyof Event)[] = ["name", "date", "location", "description", "url"];
+const TABLE_ROWS: (keyof Event)[] = [
+  "name",
+  "date",
+  "location",
+  "description",
+  "url",
+];
 
 export default async function EventsTable({ query }: { query: string }) {
   const data = await fetchEvents(query);
@@ -19,10 +25,9 @@ export default async function EventsTable({ query }: { query: string }) {
       </thead>
       <tbody>
         {data.rows.map((event) => (
-          <tr key={event.id}>
             {TABLE_ROWS.map((row) => (
               <td key={event.id + row}>
-                <p>{event[row].toString()}</p>
+                <p>{event[row] ? event[row].toString() : ""}</p>
               </td>
             ))}
           </tr>
