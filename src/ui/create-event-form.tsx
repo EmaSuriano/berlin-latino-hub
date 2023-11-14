39f85ac6a225661238ada26cd7320e1f145a4980
antoniosaleme <kilo@disroot.org>
2023-11-14 18:27:26 +0100

    refactor: add grid to form

--- src/ui/create-event-form.tsx
+++ src/ui/create-event-form.tsx
@@ -15,7 +15,7 @@ const FIELDS: Record<keyof EventCreation, EventComponent> = {
   description: TextArea,
   url: TextInput,
   category: TextInput,
-  image: TextInput
+  image: TextInput,
 };
 
 export default function CreateEventForm() {
@@ -24,29 +24,31 @@ export default function CreateEventForm() {
 
   return (
     <form action={dispatch}>
-      <div className="rounded-md bg-gray-50 p-4 dark:bg-gray-900 md:p-6">
+      <div className="grid grid-cols-1 gap-5 rounded-md bg-gray-200 p-4 dark:bg-gray-900 md:grid-cols-2 md:p-6">
         {Object.entries(FIELDS).map(([name, Component]) => {
           const key = name as keyof EventCreation;
           return (
-            <Component
-              key={key}
-              name={key}
-              errors={(state.errors && state.errors[key]) || []}
-            />
+            <div key={key} className="capitalize">
+              <Component
+                name={key}
+                errors={(state.errors && state.errors[key]) || []}
+              />
+            </div>
           );
         })}
 
-        {state.message ? (
-          <div aria-live="polite" className="my-2 text-sm text-red-500">
+        {state.message && (
+          <div
+            aria-live="polite"
+            className="col-span-full my-2 text-sm text-red-500"
+          >
             <p>{state.message}</p>
           </div>
-        ) : null}
-      </div>
+        )}
 
-      <div className="mt-6 flex justify-end gap-4">
         <button
           type="submit"
-          className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
+          className="col-span-full mt-2 flex h-10 w-full items-center justify-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
         >
           Create
         </button>
