22404261ac1c2c3787666ef849fb25a387131bdf
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-11-15 09:59:14 +0100

    fix build

--- scripts/create-local-db.ts
+++ scripts/create-local-db.ts
@@ -1,6 +1,13 @@
 import { PLACEHOLDER_EVENTS } from "../src/lib/placeholder-data";
 import { writeFileSync } from "fs";
 
+/**
+ * Setup local file called storage.json where we have local events
+ * This is useful for:
+ *  - You have no internet and wants to run the project
+ *  - We want to run the project without credentials (or we receive contributions)
+ *  - We want to run tests in CI (because we don't have the .env in Github (yet))
+ */
 async function main() {
   return writeFileSync(
     "./storage.json",
